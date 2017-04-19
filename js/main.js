70706f3c162743edf9eeed4fa640f6bfa4b5b1eb
gitbooster <webtheory@mail.ru>
2017-04-19 13:48:44 +0300

    Add dinamic input

--- js/main.js
+++ js/main.js
@@ -52,3 +52,19 @@ $(document).ready(function () {
     'container':'body'
   });
 });
+
+ var room = 1;
+function education_fields() {
+ 
+    room++;
+    var objTo = document.getElementById('education_fields')
+    var divtest = document.createElement("div");
+	divtest.setAttribute("class", "form-group-added removeclass"+room);
+	var rdiv = 'removeclass'+room;
+    divtest.innerHTML = '<div class="row"><div class="col-xs-4"><input type="text" class="form-control input-lg" placeholder="Пользователь откуда парсим"></div><div class="col-xs-4"><input type="text" class="form-control input-lg" placeholder="Aльбом, куда парсим."></div><div class="col-xs-3"><input type="text" class="form-control input-lg" placeholder="Наценка"></div><div class="col-xs-1"> <button class="btn btn-danger input-lg" type="button" onclick="remove_education_fields('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div>';
+    
+    objTo.appendChild(divtest)
+}
+   function remove_education_fields(rid) {
+	   $('.removeclass'+rid).remove();
+   }
